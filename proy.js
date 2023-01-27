fe2eb0bc3b50223e62c8fb2c2bb6872056d6fbd4
julian rendo <juli_tepa@hotmail.com>
2023-01-27 16:03:47 -0300

    poyect

---
+++ proy.js
@@ -0,0 +1,28 @@
+
+
+
+ 
+document.getElementById('boton').addEventListener('click', function() {
+    console.log('hola mundo ') 
+    document.getElementById('demo').innerHTML =  'seth.kelly@example.com'
+
+})
+document.getElementById('boton2').addEventListener('click', function() {
+    console.log('hola mundo ') 
+    document.getElementById('demo2').innerHTML =  "(855) 776-6198"
+
+})
+
+document.getElementById('boton3').addEventListener('click', function() {
+    console.log('hola mundo ') 
+    document.getElementById('demo3').innerHTML =  "valparaiso 657 b"
+
+})
+
+document.getElementById('fondo').addEventListener('click', function() {
+    document.body.style.backgroundColor = "red"
+})
+
+document.getElementById('fondo1').addEventListener('click', function() {
+    document.body.style.backgroundColor =" white"
+})
